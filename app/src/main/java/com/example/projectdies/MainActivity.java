8625d6fe85ffd46016af5e9b88c561fad4aae6b4
Revaz Bakuradze <60052562+RevazBakuradze@users.noreply.github.com>
2021-06-29 00:04:18 +0400

    Minor Adjustments to the previous commit
    Need to change main page navigation

--- app/src/main/java/com/example/projectdies/MainActivity.java
+++ app/src/main/java/com/example/projectdies/MainActivity.java
@@ -20,8 +20,6 @@ public class MainActivity extends AppCompatActivity {
     final FragmentManager fragmentManager = getSupportFragmentManager();
     private BottomNavigationView bottomNavigationView;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,17 +33,18 @@ public class MainActivity extends AppCompatActivity {
                 switch (menuItem.getItemId()) {
                     case R.id.ic_add:
                         fragment = new AddFragment();
-                        Toast.makeText(MainActivity.this, "Compose!", Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(MainActivity.this, "Compose!", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.ic_home:
                         fragment = new HomeFragment();
-                        Toast.makeText(MainActivity.this, "Home!", Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(MainActivity.this, "Home!", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.ic_user:
-                        Toast.makeText(MainActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(MainActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
                         fragment = new UserFragment();
+                        break;
                     default:
-                        fragment = new UserFragment();
+                        fragment = new HomeFragment();
                         break;
                 }
                 fragmentManager.beginTransaction().replace(R.id.fl_wrapper, fragment).commit();
