1d244e6c4b12550258fd42d377181b81819edaf5
David Xu <david.xu18@kzoo.edu>
2021-08-24 01:00:28 +0300

    edit
    fixed add fragment

--- app/src/main/java/com/dpearth/dvox/models/fragments/AddFragment.java
+++ app/src/main/java/com/dpearth/dvox/models/fragments/AddFragment.java
@@ -2,6 +2,8 @@ package com.dpearth.dvox.models.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.text.Editable;
@@ -28,42 +30,36 @@ public class AddFragment extends Fragment {
 
         super.onCreateView(inflater,container,savedInstanceState);
         View rootView = inflater.inflate(R.layout.compose_fragment, container, false);
-
-
-        word_counter = container.findViewById(R.id.word_counter);
-        content_post = container.findViewById(R.id.content_post);
-
-        content_post.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                    String text = content_post.getText().toString();
-                    text = text.replace("\n"," ");
-                    String[] textArray = text.split(" ");
-                    word_counter.setText("Words: " + textArray.length);
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
-
-        Button button = (Button) inflater.inflate(R.layout.compose_fragment, container, false);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // your handler code here
-            }
-        });
-
-        // Inflate the layout for this fragment
         return rootView;
     }
+
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            word_counter = getActivity().findViewById(R.id.word_counter);
+            content_post = getActivity().findViewById(R.id.content_post);
+            content_post.addTextChangedListener(new TextWatcher() {
+
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    String text = content_post.getText().toString();
+                    text = text.replace("\n", " ");
+                    String[] textArray = text.split(" ");
+                    word_counter.setText("Words: " + textArray.length);
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+
+                }
+
+                
+
+            });
+        }
+
 }
